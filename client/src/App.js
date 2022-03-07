bfd9b0fcf49596371371857dbee28495fe25dfc7
Kelvin Chen <32273187+kchen6195@users.noreply.github.com>
2022-03-06 20:11:45 -0500

    client

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import { useState, useEffect } from "react";
+import Axios from "axios";
+
+function App() {
+  const [listOfUsers,setListOfUsers]= useState([{id:1, name:"Kelvin",age:21,username:"kelvchen"}])
+
+  const [name,setName] = useState("")
+  const [age, setAge] = useState(0)
+  const [username,setUserName] = useState("")
+
+  useEffect(()=>{
+    Axios.get("http://localhost:3001/getUsers").then((response) => {
+      setListOfUsers(response.data)
+    })
+  },[])
+
+  const createUser = () =>(
+    Axios.post("http://localhost:3001/createUser",{name,age,username}).then((response)=>{
+      alert("User Created")
+    })
+  )
+
+  return <div className="App">
+    <div className="usersDisplay">
+      {listOfUsers.map((user)=>{
+        return(
+          <div>
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+            <h1>Username: {user.username}</h1>
+          </div>
+        )
+      })}
+      </div>
+      <div>
+        <input type="text" placeholder="Name..." onChange={(event)=>{setName(event.target.value)}}/>
+        <input type="number" placeholder="Age..." onChange={(event)=>{setAge(event.target.value)}}/>
+        <input type="text" placeholder="UserName..." onChange={(event)=>{setUserName(event.target.value)}} />
+        <button onClick={createUser}>Create User</button>
+      </div>
+    </div>;
+}
+
+export default App;
